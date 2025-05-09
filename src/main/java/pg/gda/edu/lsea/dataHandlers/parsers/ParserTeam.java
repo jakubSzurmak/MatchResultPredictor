f7e00e6ececd7a95d7151617bedb0a58aaa9fff6
Michal Sondej <s193697@student.pg.edu.pl>
2025-05-09 18:51:56 +0200

    update and delete data in the database from the console level

--- src/main/java/pg/gda/edu/lsea/dataHandlers/parsers/ParserTeam.java
+++ src/main/java/pg/gda/edu/lsea/dataHandlers/parsers/ParserTeam.java
@@ -48,7 +48,7 @@ public class ParserTeam {
 
                 if (parsedTeams.stream().noneMatch(t -> t.getId().equals(teamId))) {
                     parsedTeams.add(new Team(teamId, teamName, country));
-                    dbManager.saveToDb(parsedTeams.getLast());
+                    dbManager.saveToDb(parsedTeams.get(parsedTeams.size() - 1));
                 }
             }
 
@@ -68,7 +68,7 @@ public class ParserTeam {
 
                 if (parsedTeams.stream().noneMatch(t -> t.getId().equals(teamIdTwo))) {
                     parsedTeams.add(new Team(teamIdTwo, teamNameTwo, countryTwo));
-                    dbManager.saveToDb(parsedTeams.getLast());
+                    dbManager.saveToDb(parsedTeams.get(parsedTeams.size() - 1));
 
                 }
             }
