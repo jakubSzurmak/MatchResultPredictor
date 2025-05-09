f77982a3ea268f81b981da5edf4624f82eb6f67f
Michal Sondej <s193697@student.pg.edu.pl>
2025-05-09 18:36:39 +0200

    update and delete data in the database from the console level

--- src/main/java/pg/gda/edu/lsea/Main.java
+++ src/main/java/pg/gda/edu/lsea/Main.java
@@ -2,13 +2,41 @@ package pg.gda.edu.lsea;
 
 
 import java.io.IOException;
+import java.util.Scanner;
+import pg.gda.edu.lsea.database.DbManager;
+import java.util.List;
+
 
 
 /**
  * 1st entry point class
  */
 public class Main {
-    public static void main(String[] args) throws IOException {
-        System.out.println("Hello and welcome!");
+
+    private static void displayTeams(DbManager dbManager) {
+        Object result = dbManager.getFromDB("teams", "id", "all");
+
+        List<Object[]> resultList = (List<Object[]>) result;
+
+        System.out.println("Teams List:");
+        for (Object[] row : resultList) {
+            String teamName = (String) row[1];
+            System.out.println(teamName);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        DbManager dbManager = new DbManager();
+
+        displayTeams(dbManager);
+
+        String nameToDelete = "Bolton Wanderers";
+        dbManager.deleteFromDb("teams", "name", nameToDelete);
+
+        dbManager.updateInDb("teams", "name", "Fulham FC", "name", "Fulham");
+
+        displayTeams(dbManager);
+
     }
 }
--- src/main/java/pg/gda/edu/lsea/database/DbManager.java
+++ src/main/java/pg/gda/edu/lsea/database/DbManager.java
@@ -44,4 +44,55 @@ public class DbManager {
             return null;
         }
     }
+
+    public void updateInDb(String table, String setColumn, String setValue, String conditionColumn, String conditionValue) {
+        if (!conditionColumn.isEmpty()) {
+            EntityManager entityManager = entityManagerFactory.createEntityManager();
+            try {
+                entityManager.getTransaction().begin();
+                String query;
+                if (conditionValue.equals("all")) {
+                    query = "UPDATE " + table + " SET " + setColumn + " = '" + setValue + "'";
+                } else if (!conditionValue.isEmpty()) {
+                    query = "UPDATE " + table + " SET " + setColumn + " = '" + setValue + "' WHERE "
+                            + conditionColumn + " LIKE '%" + conditionValue + "%'";
+                } else {
+                    query = "UPDATE " + table + " SET " + setColumn + " = '" + setValue + "' WHERE "
+                            + conditionColumn + " LIKE '*'";
+                }
+                entityManager.createNativeQuery(query).executeUpdate();
+                entityManager.getTransaction().commit();
+                System.out.println("Update completed successfully.");
+            } catch (Exception e) {
+                System.out.println("Something went wrong during update: " + e.getMessage());
+            } finally {
+                entityManager.close();
+            }
+        }
+    }
+
+
+    public void deleteFromDb(String table, String conditionColumn, String conditionValue) {
+        if (!conditionColumn.isEmpty()) {
+            EntityManager entityManager = entityManagerFactory.createEntityManager();
+            try {
+                entityManager.getTransaction().begin();
+                String query;
+                if (conditionValue.equals("all")) {
+                    query = "DELETE FROM " + table;
+                } else if (!conditionValue.isEmpty()) {
+                    query = "DELETE FROM " + table + " WHERE " + conditionColumn + " LIKE '%" + conditionValue + "%'";
+                } else {
+                    query = "DELETE FROM " + table + " WHERE " + conditionColumn + " LIKE '*'";
+                }
+                entityManager.createNativeQuery(query).executeUpdate();
+                entityManager.getTransaction().commit();
+                System.out.println("Delete completed successfully.");
+            } catch (Exception e) {
+                System.out.println("Something went wrong during delete: " + e.getMessage());
+            } finally {
+                entityManager.close();
+            }
+        }
+    }
 }
